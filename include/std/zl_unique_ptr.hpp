3981ac3cce30c3d10c88ea9c12cae5989ef0618f
suncloudsmoon <gajjam2003@gmail.com>
2022-03-25 15:39:18 -0700

    Added unique_ptr<T[]> implementation

--- include/std/zl_unique_ptr.hpp
+++ include/std/zl_unique_ptr.hpp
@@ -75,6 +75,73 @@ namespace std {
 		deleter_type del;
 	};
 
+	template<typename DataType, typename Deleter>
+	class unique_ptr<DataType[], Deleter> {
+	public:
+		using pointer = DataType*;
+		using element_type = DataType;
+		using deleter_type = Deleter;
+	public:
+		explicit unique_ptr(pointer p = pointer()) noexcept : ptr(p) {}
+		unique_ptr(pointer p, const Deleter &de) noexcept : ptr(p), del(de) {}
+		unique_ptr(pointer p, Deleter &&de) noexcept : ptr(p), del(de) {}
+		unique_ptr(const unique_ptr&) = delete;
+		unique_ptr(unique_ptr &&other) noexcept {
+			ptr = other.ptr;
+			del = std::move(other.del);
+			other.ptr = nullptr;
+		}
+		~unique_ptr() { if (get()) del(get()); }
+		unique_ptr& operator=(const unique_ptr&) = delete;
+		template<typename OtherType, typename OtherDeleter>
+		unique_ptr& operator=(unique_ptr<OtherType, OtherDeleter> &&other) noexcept {
+			del(ptr);
+			ptr = other.ptr;
+			del = std::move(other.del);
+			
+			other.ptr = nullptr;
+			return *this;
+		}
+
+		pointer get() const noexcept { return ptr; }
+		deleter_type& get_deleter() noexcept { return del; }
+		const deleter_type& get_deleter() const noexcept { return del; }
+		/* Releases the ownership of the pointer */
+		pointer release() noexcept {
+			auto *temp = get();
+			get() = nullptr;
+			return temp;
+		}
+		/* Calls the deleter of existing pointer and sets the new pointer */
+		void reset(pointer p = pointer()) noexcept {
+			auto *temp = get();
+			get() = p;
+			if (temp) get_deleter()(temp);
+		}
+		void swap(unique_ptr &other) noexcept {
+			auto *temp_ptr = get();
+			auto temp_deleter = std::move(get_deleter());
+			
+			ptr = other.ptr;
+			del = std::move(other.del);
+
+			other.ptr = temp_ptr;
+			other.del = std::move(temp_deleter);
+		}
+
+		// Operators
+		explicit operator bool() const noexcept { return get(); }
+		element_type& operator[](size_t index) const { return get()[index]; }
+	private:
+		pointer ptr;
+		deleter_type del;
+	};
+
+	template<typename T, typename... Args>
+	inline unique_ptr<T> make_unique(Args&&... args) { return unique_ptr<T>{ new T{ args... } }; }
+	template<typename T>
+	inline unique_ptr<T> make_unique(size_t count) { return unique_ptr<T>{ new T[count]() }; }
+
 	template<typename FirstType, typename FirstDeleter, typename SecondType, typename SecondDeleter>
 	inline bool operator==(const unique_ptr<FirstType, FirstDeleter> &f, 
 							const unique_ptr<SecondType, SecondDeleter> &s) {
@@ -104,6 +171,11 @@ namespace std {
 	inline bool operator>=(const unique_ptr<FirstType, FirstDeleter> &f, 
 							const unique_ptr<SecondType, SecondDeleter> &s) {
 		return (f.get() >= s.get());
+	}
+
+	template<typename DataType, typename Deleter>
+	inline void swap(unique_ptr<DataType, Deleter> &f, unique_ptr<DataType, Deleter> &s) noexcept {
+		f.swap(s);
 	}
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,11 @@
 
 /* Using C-style printing functions to avoid conflicts with the C++'s std namespace */
 int main() {
-	std::unique_ptr<int> p1{ new int{1} };
+	std::unique_ptr<int> p1 = std::make_unique<int>(1);
 	std::unique_ptr<int> p2{ new int{2} };
+	std::unique_ptr<int[]> p3{ new int[3]{} }; 
+
+	std::swap(p1, p2);
 
 	printf("Pointer #1: %d\n", *p1.get());
 	printf("Pointer #2: %d\n", *p2.get());
